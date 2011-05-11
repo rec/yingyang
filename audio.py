960fcbf8c17c18dbb3f3e8b0874b0a8a0e36b27b
Tom Swirly <tom@swirly.com>
2011-05-10 21:35:21 -0400

    Tests

---
+++ audio.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python
+
+import aifc
+import wave
+
+def sampleToNum(sample):
+  """Convert a sample to an unsigned integer.
+
+  >>> sampleToNum('')
+  0
+  >>> '%x' % sampleToNum('\1\1\1\1')
+  '1010101'
+  """
+  return reduce(lambda x, y: x * 256 + ord(y), sample, 0)
+
+
+def numToSample(num, sampwidth):
+  """Convert an unsigned integer to a sample.
+
+  >>> numToSample(0, 2)
+  '\\x00\\x00'
+
+  >>> numToSample(10452, 2)
+  '(\\xd4'
+
+  >>> numToSample(3224, 2)
+  '\\x0c\\x98'
+
+  >>> numToSample(13676, 2)
+  '5l'
+"""
+  parts = []
+  for j in range(sampwidth):
+    parts.insert(0, chr(num % 256))
+    num /= 256;
+  return ''.join(parts)
+
+
+def combineFrames(nchannels, sampwidth, *frames):
+  """Combine separate frames and scale.
+
+  >>> combineFrames(1, 2, '(\\xd4', '\\x0c\\x98')
+  '5l'
+  """
+  accum = nchannels * [0]
+
+  for frame in frames:
+    for ch in range(nchannels):
+      offset = ch * sampwidth
+      accum[ch] += sampleToNum(frame[offset : offset + sampwidth])
+  return ''.join(numToSample(a / nchannels, sampwidth) for a in accum)
+
+
+def openAudio(f, perms='rb'):
+  if f.endswith('.wav'):
+    return wave.open(f, perms)
+  elif f.endswith('.aif'):
+    return aifc.open(f, perms)
+  else:
+    raise ValueError("Can't open file " + f)
+
+
+if __name__ == "__main__":
+  import doctest
+  doctest.testmod()
--- yingyang.py
+++ yingyang.py
@@ -4,8 +4,7 @@ import os.path
 import random
 import sys
 
-import aifc
-import wave
+import audio
 
 from FrameReader import FrameReader
 
@@ -24,34 +23,8 @@ def poisson(length, cuts):
     p.add(rand())
   return sorted(p)
 
-def combine(params, *frames):
-  nchannels = params['nchannels']
-  sampwidth = params['sampwidth']
-  accum = nchannels * [0]
-
-  for frame in frames:
-    for ch in range(nchannels):
-      t = 0
-      for i in range(sampwidth):
-        t = t * 256 + ord(frame[ch * sampwidth + i])
-
-      accum[ch] += t
-
-  parts = []
-  for acc in accum:
-    acc /= len(accum)
-    subparts = []
-    for j in range(sampwidth):
-      subparts.insert(0, chr(acc % 256))
-      acc /= 256;
-    parts.extend(subparts)
-
-  return ''.join(parts)
-
-
-
 def yingyang(files):
-  waves = [wave.open(f, 'rb') for f in files]
+  waves = [audio.openAudio(f, 'rb') for f in files]
 
   names = 'nchannels', 'sampwidth', 'framerate'
   params = dict((n, get_param(waves, n)) for n in names)
@@ -61,7 +34,7 @@ def yingyang(files):
   framesize = params['nchannels'] * params['sampwidth']
 
   for i, f in enumerate(files):
-    out = wave.open('-new'.join(os.path.splitext(f)), 'wb')
+    out = audio.openAudio('-new'.join(os.path.splitext(f)), 'wb')
     for name in names:
       getattr(out, 'set' + name)(params[name])
 
@@ -70,7 +43,10 @@ def yingyang(files):
     mirror = FrameReader(framesize, frames[m], cuts[m], n, True)
 
     for sample in range(n):
-      out.writeframes(combine(params, reader.nextFrame(), mirror.nextFrame()))
+      out.writeframes(audio.combineFrames(params['nchannels'],
+                                          params['sampwidth'],
+                                          reader.nextFrame(),
+                                          mirror.nextFrame()))
 
     out.close()
 
